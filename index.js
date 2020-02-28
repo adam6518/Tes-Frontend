066b7483864307baab774ceb71c67380b21c2eb6
adam6518 <getamped75@gmail.com>
2020-02-28 22:59:10 +0700

    tes-frontend

---
+++ index.js
@@ -0,0 +1,52 @@
+var kost = [
+    {
+        namaKost: '',
+        harga: 1500000,
+        kontak: '@RegentownBSD',
+        jumlahRumah: 2,
+        jumlahKamar: 10
+    },
+    {
+        namaKost: '',
+        harga: 1200000,
+        kontak: '@ClusterAllogio',
+        jumlahRumah: 2,
+        jumlahKamar: 13
+    },
+    {
+        namaKost: 'Anartha House',
+        harga: 2000000,
+        kontak: '@VanyaParkBSD',
+        jumlahRumah: 5,
+        jumlahKamar: 30
+    },
+    {
+        namaKost: 'Kost Alesha',
+        harga: 1500000,
+        kontak: '@VanyaParkBSD',
+        jumlahRumah: 1,
+        jumlahKamar: 7
+    },
+    {
+        namaKost: '',
+        harga: 1000000,
+        kontak: '@ClusterAllogia',
+        jumlahRumah: 1,
+        jumlahKamar: 8
+    },
+    {
+        namaKost: '',
+        harga: 1300000,
+        kontak: '@VanyaParkBSD',
+        jumlahRumah: 2,
+        jumlahKamar: 14
+    }
+]
+
+let show = () => {
+    let list = kost.map((dataKost) => {
+        return `<div>
+            <img />
+        </div>`
+    })
+}
